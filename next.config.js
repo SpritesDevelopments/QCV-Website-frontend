29cf559a5828150cc5f19aca03a59557431db848
Sprite Developments <123204447+SpritesDevelopments@users.noreply.github.com>
2026-04-05 14:17:32 +0100

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -9,11 +9,25 @@ const nextConfig = {
         pathname: '/api/uploads/**',
       },
       {
+        protocol: 'http',
+        hostname: '188.165.254.184',
+        port: '8106',
+        pathname: '/api/uploads/**',
+      },
+      {
         protocol: 'https',
         hostname: '**',
         pathname: '/api/uploads/**',
       },
     ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://188.165.254.184:8106/api/:path*',
+      },
+    ];
   },
 };
 
